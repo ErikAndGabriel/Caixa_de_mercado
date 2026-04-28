d80816ae38c4d0447d00cb0637383c617570fa95
erikandgabriel <eeu31471@gmail.com>
2026-04-28 01:02:12 -0300

    Implement main menu and product addition functionality

---
+++ start.py
@@ -0,0 +1,55 @@
+from src.visualizar import Visualizar
+from src.carrinho import Carrinho
+from core.apagar import clear
+import os
+     
+def NovoProduto():
+    while True:
+        try:
+            clear()
+            print("""
+            Atenção, adicionar codigo em seguida  quantidade, para sair do programa e so digitar 000""")
+            codigo = int(input("codigo: "))                
+            quantidade = int(input("quantidade: ")) 
+        except ValueError:
+             print("somente numeros!")
+             input("precione [Enter]")
+             continue
+        if codigo == 000 or quantidade == 000:
+           break
+        else:
+            pessoa = Carrinho(codigo, quantidade)
+            pessoa.Adicionar_no_carrinho_codigo()
+            print(pessoa.olhar_lista_carrinho())
+            input()
+            continue
+                
+                                
+def Menu():
+    while True:
+        try:
+            clear()
+            print("[1] olhar produtos")
+            print("[2] nova compra")
+            print("[0] exit")
+            escolha = int(input("\nescolha: "))
+        
+            if escolha == 1:
+                pessoa = Visualizar()
+                pessoa.Lista()
+                input("\nprecione [ENTER]")
+            elif escolha == 2:
+                NovoProduto()
+                                 
+            elif escolha == 0:
+               exit()
+                
+            else:
+                print("escolha invalida")
+                input("precione [ENTER]")
+                continue
+        except ValueError:
+            print("somente numeros")
+            input("precione [ENTER]")
+            continue
+Menu()
