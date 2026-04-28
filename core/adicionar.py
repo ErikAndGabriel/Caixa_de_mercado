6f2cfca1fe4a93d2202bc787634f68c15977c8eb
erikandgabriel <eeu31471@gmail.com>
2026-04-28 01:04:13 -0300

    Add files via upload

---
+++ core/adicionar.py
@@ -0,0 +1,8 @@
+import json
+import os 
+
+def adicionar_produto(dados):
+    pasta_atual = os.path.dirname(os.path.obspath(__file__))
+    arquivo = os.path.join(pasta_atual, "..", "dados", "mercadorias.json")
+    with open(arquivo, "w") as arq:
+        json.dump(dados, arq, indent=4)
