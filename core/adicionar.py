8a2aa0f3719a6d3b40c02475d5de6d1b50104b56
erikandgabriel <eeu31471@gmail.com>
2026-05-04 02:29:47 -0300

    Update adicionar.py

--- core/adicionar.py
+++ core/adicionar.py
@@ -1,8 +1,9 @@
 import json
 import os 
 
+pasta_atual = os.path.dirname(os.path.abspath(__file__))
+
 def adicionar_carrinho(dados):
-    pasta_atual = os.path.dirname(os.path.abspath(__file__))
     arquivo = os.path.join(pasta_atual, "..", "dados", "carrinho.json")
     with open(arquivo, "w") as arq:
         json.dump(dados, arq, indent=4)
