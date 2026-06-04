db4972eaa6509c04aeecf0a3272b259b81f58fd0
erikandgabriel <eeu31471@gmail.com>
2026-06-04 00:28:01 -0300

    Update remover.py

--- core/remover.py
+++ core/remover.py
@@ -7,8 +7,8 @@ def remover_produto(id_produto):
     arquivo = os.path.join(pasta_atual, "..", "dados", "carrinho.json")
     with open(arquivo, "r") as arq:
         data = json.load(arq)
-    for p in data:
-        if p == id_produto:
-           p['cancelamento'] == True
+    id_produto = str(id_produto)
+    if id_produto in data:
+        data[id_produto]['cancelamento'] == True
     with open(arquivo, "w") as arq:
         json.dump(data, arq, indent=4)
