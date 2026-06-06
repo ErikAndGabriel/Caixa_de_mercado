5fd722d53ca7dc43f4879e329cede163f0d67e56
erikandgabriel <eeu31471@gmail.com>
2026-06-06 16:54:30 -0300

    Add enumeration to product listing in visualizar.py

--- src/visualizar.py
+++ src/visualizar.py
@@ -3,11 +3,13 @@ from colorama import init, Fore
 from core.carregar import carregar_sql 
 init()
 
+enumerar = 0
 class Visualizar:
     def __init__(self, dados = carregar_sql()):
         self.dados = dados 
         
     def Lista(self):
         for info, produto in self.dados.items():
-            print(f"produto: {info}  codigo: {produto['codigo']}")  
+            enumerar += 1 
+            print(f"{enumerar} produto: {info}  codigo: {produto['codigo']}")  
              
