b59d96345455b9a7caf41fac006390d9b183df11
erikandgabriel <eeu31471@gmail.com>
2026-06-06 02:01:22 -0300

    Add initial menu structure for UI

---
+++ ui/menus.py
@@ -0,0 +1,21 @@
+menu_inicial = """
+[1] olhar produtos
+[2] nova compra
+[3] info
+[0] sair
+"""
+
+menu_nova_compra = """
+[1] resetar
+[2] resetar e sair
+[3] cancelar um item
+"""
+
+menu_forma_de_pagamento = """
+[1] dinheiro
+[2] pix
+[3] crédito
+[4] débito
+[5] alimentação
+[6] continuar compra
+"""
