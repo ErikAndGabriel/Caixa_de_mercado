369c6e496d728ef6d6831c5df4773eb1e03c333c
erikandgabriel <eeu31471@gmail.com>
2026-06-10 23:45:04 -0300

    Update visualizar.py

--- src/visualizar.py
+++ src/visualizar.py
@@ -9,9 +9,5 @@ class Visualizar:
         
     def Lista(self):
         for info, produto in self.dados.items():
-            self.enumerar += 1
-            if self.dados[produto]["cancelamento"] == True:
-                print(f"{erro}{self.enumerar} produto: {info}  codigo: {produto['codigo']}")  
-            else:
-                print(f"{sucesso}{self.enumerar} produto: {info}  codigo: {produto['codigo']}")  
+            print(f"{sucesso}{self.enumerar} produto: {info}  codigo: {produto['codigo']}")  
 
