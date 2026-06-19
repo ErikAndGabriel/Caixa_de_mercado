f1845c375dc1a8e2616143403f4e7c39281a9a1f
erikandgabriel <eeu31471@gmail.com>
2026-06-19 09:25:28 -0300

    Update menus.py

--- ui/menus.py
+++ ui/menus.py
@@ -11,50 +11,50 @@ menu_nova_compra = f"""
 {ciano}[2] {branco}resetar e sair
 {ciano}[3] {branco}cancelar um item
 """
-menu_admin = """
-[1] produtos
+menu_admin = f"""
+{ciano}[1] produtos
 [2] admins
 [3] users
 [0] sair
 """
-menu_admin_produto = """
+menu_admin_produto = f"""
 [1] olhar produtos
 [2] adicionar produtos
 [3] editar produtos
 [4] remover produto
 [0] voltar
 """
-menu_admin_admin = """
+menu_admin_admin = f"""
 [1] olhar admins
 [2] remover admin
 [3] adicionar admin
 [4] aditar admin
 [0] voltar
 """
-menu_admin_users = """
+menu_admin_users = f"""
 [1] olhar users
 [2] olhar info de user
 [3] remover user
 [4] adicionar user
 [0] voltar
 """
-menu_pos_compra = """
+menu_pos_compra = f"""
 [1] adicionar cpf
 [2] continuar 
 [0] voltar
 """
 
-menu_cpf = """
+menu_cpf = f"""
 [1] desconto
 [2] alimentação no cpf
 [0] voltar
 """
-menu_forma_de_pagamento = """
-[1] dinheiro
-[2] pix
-[3] crédito
-[4] débito
-[5] alimentação
-[6] continuar compra
-[0] voltar 
+menu_forma_de_pagamento = f"""
+{ciano}[1] {branco}dinheiro
+{ciano}[2] {branco}pix
+{ciano}[3] {branco}crédito
+{ciano}[4] {branco}débito
+{ciano}[5] {branco}alimentação
+{ciano}[6] {branco}continuar compra
+{ciano}[0] {branco}voltar 
 """
